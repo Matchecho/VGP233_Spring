d7add58ea1a0e09592f80c8fa3357ddbdbf2d277
Mateo Sergio Hernández <matchecho5@gmail.com>
2021-04-26 22:25:54 -0700

    Assignment 2

---
+++ Mateo_Hernandez_Assignment2/Assets/Assignment2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float DelayOfDog = 0.0f; //Delay of 2.5 seconds
+    // Update is called once per frame
+    void Update()
+    {        
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && DelayOfDog <= 0.0f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            DelayOfDog = 150.0f;
+        }
+        if (DelayOfDog > 0.0f)
+        {
+            DelayOfDog--;
+        }        
+    }
+}
---
+++ Mateo_Hernandez_Assignment2/Assets/Assignment2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -20;
+    private float spawnLimitXRight = 9;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnInterval = Random.Range(3.0f, 5.0f);
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        int BallInd = Random.Range(0, 3);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[BallInd], spawnPos, ballPrefabs[BallInd].transform.rotation);
+    }
+
+}
